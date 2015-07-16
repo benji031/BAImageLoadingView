94ec4c3db7ba64d65df2424bb9f9ea04ef3b40c7
Benjamin DENEUX <benjamin.deneux@bananapps.fr>
2015-07-16 10:32:36 +0200

    Initial Commit

---
+++ BAImageLoadingExemple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BAImageLoadingExemple
+//
+//  Created by Benjamin Deneux on 16/07/2015.
+//  Copyright (c) 2015 Bananapp's. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
